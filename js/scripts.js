7ead840ee2a0c048ce88453d0bf660056750db32
tanywu4ka <tanywu4ka@ukr.net>
2014-10-21 23:42:51 +0300

    Revert "LATSD/TASK3 : main menu and index.html"
    This reverts commit aaf1c23b464713386c460848cb946f71998b65bf.

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,6 @@
-$(function () {
-    $('.menu-icon-1').tooltip();
-    $('.menu-icon-2').tooltip();
-    $('.menu-icon-3').tooltip();
-    $('.menu-icon-4').tooltip();
-    
-    $("#menu button").click(function(){
-       $("#menu").css("background-color","rgb(55,55,55)"); 
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
     });
-    
-    $(".menu-icon-1").click(function(){
-        $("#initial-view").hide();
-        $("#font-option").hide(); 
-        $("#get-quote-view").hide();
-        $("#product-view").show();
-    });
-    $(".menu-icon-2").click(function(){
-        $("#font-option").fadeIn("slow");
-    });
-    $(".menu-icon-3").click(function(){
-        
-    });
-    $(".menu-icon-4").click(function(){
-        
-    });
-    
-    $("#quote").click(function(){
-        $("#menu").hide();
-        $("#initial-view").hide();
-        $("#font-option").hide(); 
-        $("#get-quote-view").show();
-    });
-    
-    $(".glyphicon-remove-circle").click(function(){
-        initialView();
-    });
-});
 
 
-function initialView(){
-    $("#initial-view").show();
-    $("#font-option").hide(); 
-    $("#get-quote-view").hide();
-    $("#product-view").hide();
-    $("#menu").css("background-color","rgb(255,255,255)"); 
-}
